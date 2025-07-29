e2f5b9d26df22aaf198961ab05e83f56deaf5712
Aanya <aanya.malyala@gmail.com>
2025-07-29 16:16:03 -0400

    Initial Commit

---
+++ FinalProjectGoalTracker/FinalProjectGoalTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectGoalTrackerApp.swift
+//  FinalProjectGoalTracker
+//
+//  Created by Aanya Malyala on 7/29/25.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectGoalTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
