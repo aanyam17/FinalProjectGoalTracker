d4dd332fb5af38246442ce1b67747bc7d5bb2511
Aanya <aanya.malyala@gmail.com>
2025-07-30 14:41:45 -0400

    Added button to add new goal, used theme colors, added NewGoalView.

--- FinalProjectGoalTracker/CreateGoalView.swift
+++ FinalProjectGoalTracker/CreateGoalView.swift
@@ -13,9 +13,20 @@ struct CreateGoalView: View {
                 Text("My Goals")
                     .font(.system(size: 40))
                     .fontWeight(.black)
-                    .foregroundColor(Color.pink)
+                    .foregroundColor(Color(red: 0.886, green: 0.612, blue: 0.620))
             Spacer()
+            Button(action:  {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }) {
+                Text("Add New Goal")
+                    .font(.headline)
+                    .foregroundColor(Color(red: 0.710, green: 0.086, blue: 0.196)) //text color
+                    .padding()
+                    .background(Color(red: 0.886, green: 0.612, blue: 0.620)) //background color
+                    .cornerRadius(12) //rounded edges
+            }
         }
+        .padding()
         
     }
 }
