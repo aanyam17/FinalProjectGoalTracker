77782e1f6884693933273c72ca2cc0f2236a2040
Roshal <roshalsebastian10@gmail.com>
2025-07-30 12:46:53 -0500

    added navigation stacks to second view

--- FinalProjectGoalTracker/ContentView.swift
+++ FinalProjectGoalTracker/ContentView.swift
@@ -9,17 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Color ("AppBackground") .edgesIgnoringSafeArea(.all)
-            VStack (spacing: 40) {
-                Text("Dear Goal")
-                    .font(.title)
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(.brown)
-                    .shadow(radius: 2)
+        NavigationStack{
+            ZStack {
                 
+                
+                VStack (spacing: 40) {
+                    Text("AIM")
+                        .font(.title)
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.brown)
+                        .shadow(radius: 2)
+                    Text("A goal tracker as simple as your ambitions are big!")
+                        .foregroundColor(.black)
+                        .multilineTextAlignment(.center)
+                    NavigationLink(destination: SecondView()) {
+                        Text("Click me")
+                            .bold()
+                            .foregroundStyle(.pink)
+                        
+                    }
+                    .padding()
+                }
             }
-            .padding()
         }
     }
 }
