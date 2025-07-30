1a33790ec83574dcbb341875e15114a8ecc410c9
Roshal <roshalsebastian10@gmail.com>
2025-07-30 13:39:57 -0500

    added stylistic edits to text and elements

--- FinalProjectGoalTracker/ContentView.swift
+++ FinalProjectGoalTracker/ContentView.swift
@@ -17,16 +17,32 @@ struct ContentView: View {
                 VStack (spacing: 40) {
                     Image("logo")
                         .cornerRadius(45)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 45)
+                                .stroke(Color.black, lineWidth: 0.5)
+                            )
+                        .shadow(color: .black.opacity(0.3), radius: 4, x: 2, y: 4)
                     Text("A goal tracker as simple as your ambitions are big!")
                         .foregroundColor(.black)
                         .multilineTextAlignment(.center)
                     NavigationLink(destination: SecondView()) {
-                        Text("Click me")
+                        Text("let's get started")
                             .bold()
-                            .foregroundStyle(.pink)
-                        
+                            .foregroundStyle(.black)
+                            .background(
+                              RoundedRectangle(cornerRadius: 45)
+                                .fill(Color.pink.opacity(0.3))
+                                .frame(width: 150, height: 90)
+                                .overlay(
+                                        RoundedRectangle(cornerRadius: 45)
+                                            .stroke(Color.black, lineWidth: 0.5)
+                                        
+                                
+                        )
+                                .shadow(color: .black.opacity(0.3), radius: 4, x: 2, y: 4)
+                              )
                     }
-                    .padding()
+                    .padding(60)
                 }
             }
         }
